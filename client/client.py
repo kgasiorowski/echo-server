6792920966caf749e12a8fdfbea1183f9af5d055
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2020-04-07 21:38:39 -0400

    Add some error handling

--- client/client.py
+++ client/client.py
@@ -7,13 +7,17 @@ PORT = secret.PORT
 if __name__ == "__main__":
 
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((HOST, PORT))
 
-	data = 'temp'
+	try:
+		sock.connect((HOST, PORT))
+	except ConnectionRefusedError:
+		exit("Connection refused: is the server running?")
+
+	data = 'temporary data value'
 
 	while data != '':
 		data = sock.recv(2048).decode()
-		print(data, end='')
+		print(data, flush=True, end='')
 
 	sock.close()
 
